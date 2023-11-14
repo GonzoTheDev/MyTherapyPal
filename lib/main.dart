ed0887621335849b70ea6db438e17da68c130223
GonzoTheDev <c20703429@mytudublin.ie>
2023-11-14 15:55:25 +0000

    login setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,128 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 void main() {
-	runApp(NoteApp());
+	runApp(MainApp());
 }
-class NoteApp extends StatelessWidget {
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
 	@override
 	Widget build(BuildContext context) {
 		return MaterialApp(
-			title: 'Note Taking App',
+			title: 'MyTherapyPal',
 			theme: ThemeData(
-				primarySwatch: Colors.blue,
+				primarySwatch: Colors.cyan,
+        scaffoldBackgroundColor: Color.fromARGB(255, 238, 235, 235),
 			),
-			home: NoteHomePage(),
+			home: const Login(title: 'MyTherapyPal'),
 		);
 	}
 }
+class Login extends StatefulWidget {
+  final String title;
+  const Login({Key? key, required this.title}) : super(key: key);
+	@override
+	_LoginState createState() => _LoginState();
+}
+class _LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+            widget.title,
+            style: const TextStyle(color: Colors.white),
+          ),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Email"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Password"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16.0),
+                child: Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        // Navigate the user to the Home page
+                        if (emailController.text == "sbw92@outlook.com" && passwordController.text == "test") {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => NoteHomePage(
+                                title: 'Notes Page',
+                                email: emailController.text,
+                            )),
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Invalid Credentials')),
+                            );
+                        }
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Please fill input')),
+                        );
+                      }
+                    },
+                    child: const Text('Submit', style: TextStyle(color: Colors.white),),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
 class NoteHomePage extends StatefulWidget {
+  const NoteHomePage({super.key, required this.email, required this.title});
+  final String email;
+  final String title;
 	@override
 	_NoteHomePageState createState() => _NoteHomePageState();
 }
@@ -57,10 +164,26 @@ class _NoteHomePageState extends State < NoteHomePage > {
 	Widget build(BuildContext context) {
 		return Scaffold(
 			appBar: AppBar(
-				title: Text('Note Taking App'),
+				title: const Text(
+            'MyTherapyPal',
+            style: TextStyle(color: Colors.white),
+          ),
 			),
-			body: Column(
+			body: Column( 
 				children: < Widget > [
+          Text(
+            widget.title,
+            style: TextStyle(fontSize: 20),
+          ),
+          Text(widget.email),
+          Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Go back!"),
+              ),
+          ),
 					Expanded(
 						child: ListView.builder(
 							itemCount: _notes.length,
