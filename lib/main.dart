dd31c978d683bcd441e4f6199cc74f96100c71f5
GonzoTheDev <c20703429@mytudublin.ie>
2023-11-18 19:11:43 +0000

    Login & Registration
    Further implementation of login and registration

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_sizer/flutter_sizer.dart';
 
 FirebaseAuth auth = FirebaseAuth.instance;
 
@@ -13,6 +14,14 @@ void main() async{
   );
 	runApp(MainApp());
   
+}
+
+passwordMatch(pwd1, pwd2) {
+  if (pwd1 == pwd2) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Create a user in firebase
@@ -35,10 +44,17 @@ createUser(user, pass) async {
 
 // Login a user in firebase
 loginUser(user, pass) async {
-  UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+
+  await FirebaseAuth.instance.signInWithEmailAndPassword(
     email: user,
     password: pass
   );
+  
+}
+
+// Logout a user in firebase
+logoutUser() async {
+  await FirebaseAuth.instance.signOut();
 }
 
 // Main app widget
@@ -46,15 +62,18 @@ class MainApp extends StatelessWidget {
   const MainApp({Key? key}) : super(key: key);
 	@override
 	Widget build(BuildContext context) {
-		return MaterialApp(
-			title: 'MyTherapyPal',
-			theme: ThemeData(
-				primarySwatch: Colors.cyan,
-        scaffoldBackgroundColor: Color.fromARGB(255, 238, 235, 235),
-			),
-			home: const Login(title: 'MyTherapyPal'),
-		);
-	}
+    return FlutterSizer(
+      builder: (context, orientation, screenType) {
+      return MaterialApp(
+        title: 'MyTherapyPal',
+        theme: ThemeData(
+          primarySwatch: Colors.cyan,
+          scaffoldBackgroundColor: Color.fromARGB(255, 238, 235, 235),
+        ),
+        home: const Login(title: 'MyTherapyPal'),
+      );
+	});
+  }
 }
 
 // Login widget
@@ -68,6 +87,137 @@ class _LoginState extends State<Login> {
   final _formKey = GlobalKey<FormState>();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+            widget.title,
+            style: const TextStyle(color: Colors.white),
+          ),
+      ),
+      body: Form(
+        key: _formKey,
+        child: 
+        Padding(
+          padding: const EdgeInsets.only(top: 32, bottom: 32, left: 8, right: 8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                'Login',
+                style: TextStyle(color: Colors.black, fontSize: 20),
+              ),
+                Container(
+                  padding:
+                    const EdgeInsets.only(top: 32, bottom: 8, left: 8, right: 8),
+                  width: Adaptive.w(50),
+                  child: TextFormField(
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), labelText: "Email"),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+              Container(
+                padding:
+                    const EdgeInsets.only(top: 8, bottom: 32, left: 8, right: 8),
+                width: Adaptive.w(50),
+                child: TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Password"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.only(top: 8, bottom: 8, left: 8, right: 8),
+                child: Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        loginUser(emailController.text, passwordController.text);
+                        User? user = FirebaseAuth.instance.currentUser;
+                        // Navigate the user to the Home page
+                          if (user != null) {
+                            Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => NoteHomePage(
+                                title: 'Notes Page',
+                                email: emailController.text,
+                            )),
+                          );
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Invalid Credentials')),
+                            );
+                          }
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Please fill input')),
+                        );
+                      }
+                    },
+                    child: const Text('Submit', style: TextStyle(color: Colors.white),),
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.only(top: 4, left: 8, right: 8),
+                child: Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const Register(
+                                title: 'Register',
+                            )),
+                          );
+                        }, 
+                    child: const Text('Register', style: TextStyle(color: Colors.white),),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Register widget
+class Register extends StatefulWidget {
+  final String title;
+  const Register({Key? key, required this.title}) : super(key: key);
+	@override
+	_RegisterState createState() => _RegisterState();
+}
+class _RegisterState extends State<Register> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController fnameController = TextEditingController();
+  TextEditingController snameController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController pwd1Controller = TextEditingController();
+  TextEditingController pwd2Controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -104,13 +254,29 @@ class _LoginState extends State<Login> {
                 padding:
                     const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
                 child: TextFormField(
-                  controller: passwordController,
+                  controller: pwd1Controller,
                   obscureText: true,
                   decoration: const InputDecoration(
                       border: OutlineInputBorder(), labelText: "Password"),
                   validator: (value) {
                     if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
+                      return 'Please enter a password';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  controller: pwd2Controller,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Confirm Password"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please confirm your password';
                     }
                     return null;
                   },
@@ -123,24 +289,27 @@ class _LoginState extends State<Login> {
                   child: ElevatedButton(
                     onPressed: () {
                       if (_formKey.currentState!.validate()) {
-                        loginUser(emailController.text, passwordController.text);
-                        User? user = FirebaseAuth.instance.currentUser;
-                        // Navigate the user to the Home page
-                        if (user!= null) {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => NoteHomePage(
-                                title: 'Notes Page',
-                                email: emailController.text,
-                            )),
-                          );
-                        } else {
-                          createUser(emailController.text, passwordController.text );
-                          /*ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text('Invalid Credentials')),
-                            );*/
+                        if(passwordMatch(pwd1Controller.text, pwd2Controller.text)){
+                          createUser(emailController.text, pwd1Controller.text);
+                          User? user = FirebaseAuth.instance.currentUser;
+                          // Navigate the user to the Home page
+                          if (user!= null) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const Login(
+                                  title: 'Login Page',
+                              )),
+                            );
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('User created successfully, please check your email to confirm your account!')),
+                            );
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('Password does not match')),
+                              );
+                          }
                         }
                       } else {
                         ScaffoldMessenger.of(context).showSnackBar(
@@ -217,15 +386,6 @@ class _NoteHomePageState extends State < NoteHomePage > {
             widget.title,
             style: TextStyle(fontSize: 20),
           ),
-          Text(widget.email),
-          Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text("Go back!"),
-              ),
-          ),
 					Expanded(
 						child: ListView.builder(
 							itemCount: _notes.length,
@@ -253,8 +413,28 @@ class _NoteHomePageState extends State < NoteHomePage > {
 							),
 						),
 					),
+          Padding(
+                padding:
+                    const EdgeInsets.only(top: 4, bottom: 8, left: 8, right: 8),
+                child: Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                          logoutUser();
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const Login(
+                                title: 'Login Page',
+                            )),
+                          );
+                        }, 
+                    child: const Text('Logout', style: TextStyle(color: Colors.white),),
+                  ),
+                ),
+              ),
 				],
 			),
 		);
 	}
 }
+
