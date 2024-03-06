4d1c7a601c9cb96f9b0c6cf69ac530abf1774b18
GonzoTheDev <c20703429@mytudublin.ie>
2024-03-06 21:43:45 +0000

    Increase RSA keysize to 4096

--- lib/services/encryption/RSA/rsa.dart
+++ lib/services/encryption/RSA/rsa.dart
@@ -33,7 +33,7 @@ class RSAEncryption {
 
   generateRSAKeyPair() {
     print("Generating RSA key pair...");
-    final rsaKeypair = RSAKeypair.fromRandom();
+    final rsaKeypair = RSAKeypair.fromRandom(keySize: 4096);
     print("RSA key pair generated...");
     return (
       publicKey: rsaKeypair.publicKey.toPEM(),
